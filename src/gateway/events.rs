4290b3c0128ca40a3a5df4cb6d04eb9f29918560
Tiago Dinis Ribeiro Dos Santos <tiagodinis33@gmail.com>
2022-06-25 12:03:04 +0000

    add user field to ready event

--- src/gateway/events.rs
+++ src/gateway/events.rs
@@ -1,20 +1,11 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+use crate::{user::User, guild::UnavailableGuild};
+
+#[derive(Deserialize, Debug, Clone)]
 pub struct ReadyEvent {
     pub v: i32,
     pub session_id: String,
-    pub guilds: Vec<crate::guild::UnavailableGuild>,
-}
-impl Clone for ReadyEvent {
-    fn clone_from(&mut self, source: &Self)
-    {
-        self.v = self.v.clone();
-        self.session_id = self.session_id.clone();
-        self.guilds = self.guilds.clone();
-    }
-
-    fn clone(&self) -> Self {
-        Self { v: self.v.clone(), session_id: self.session_id.clone(), guilds: self.guilds.clone() }
-    }
+    pub guilds: Vec<UnavailableGuild>,
+    pub user: User
 }
--- src/user.rs
+++ src/user.rs
@@ -5,16 +5,16 @@ pub enum PremiumType {
     NitroClassic = 1,
     Nitro = 2,
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct User {
-    pub id: u64,
+    pub id: String,
     pub username: String,
     pub discriminator: String,
     pub bot: bool,
-    pub system: bool,
+    pub system: Option<bool>,
     pub mfa_enabled: bool,
-    pub accent_color: u32,
-    pub locale: String,
+    pub accent_color: Option<u32>,
+    pub locale: Option<String>,
     pub verified: bool,
-    pub premium_type: u8
+    pub premium_type: Option<u8>
 }
