75ea5823af474ba1ee9f4d23706d4edcf0983905
Tiago Dinis Ribeiro Dos Santos <tiagodinis33@gmail.com>
2022-06-26 11:11:32 +0000

    Reset project

--- examples/hello_world_bot.rs
+++ examples/hello_world_bot.rs
@@ -1,15 +1,3 @@
-use disrust::events::ReadyEvent;
-
-#[tokio::main]
-async fn main() {
-    let token = option_env!("TOKEN");
-    let mut bot = disrust::Bot::new(
-        token.expect("No token was provided at compile time"),
-        disrust::GatewayIntents::Guilds as i32,
-    )
-    .await;
-    bot.on("ready", |x| {
-        println!("Ready: {:#?}", x.as_ref().downcast_ref::<ReadyEvent>().unwrap());
-    });
-    bot.login().await;
+fn main() {
+   
 }
